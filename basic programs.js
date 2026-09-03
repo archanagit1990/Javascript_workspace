f9b9658b878969d7b51020929296cae966d81382
Archana Rajendran <archanaraj12@gmail.com>
2026-09-03 22:55:28 +0530

    basic programs

---
+++ basic programs.js
@@ -0,0 +1,15 @@
+/* # print 
+let name="Archana"
+let experiance=15
+console.log ("Name is ",name,"and experiance is ",experiance)
+ */
+
+let actpods=5
+let exppods=5
+if (actpods==exppods)
+{
+    console.log ("Build is HEALTHY")
+}
+else {
+    console.log ("Build is UNHEALTHY")
+}
